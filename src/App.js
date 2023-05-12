92aa9c83b75aa8120b0019050f9069b27b079d33
Jared Ciccarello <Jared.Ciccarello@gmail.com>
2023-05-11 21:19:17 -0400

    Created submit button

--- src/App.js
+++ src/App.js
@@ -1,59 +1,107 @@
-import React from 'react';
-import Header from './Header.js';
+// This is the parent file
 
-import Container from 'react-bootstrap/Container';
+import React from 'react';
+// listGroup component is Bootstrap's version of UL
+import ListGroup from 'react-bootstrap/ListGroup';
+import Button from 'react-bootstrap/Button';
 import Form from 'react-bootstrap/Form';
-import './App.css';
+
+let data = [1,2,3,4,5,6,7,8,9,10];
 
 class App extends React.Component {
-
   constructor(props) {
     super(props);
-
     this.state = {
-      username: 'user'
-    };
-  }
-  handleFormSubmitted = e => {
-    e.preventDefault();
-    console.log('submitted');
-  }
-  handleUsernameTyped = e => {
-    // e.preventDefault();
-    if(e.target.value.length > 0) {
-      this.setState({
-        username: e.target.value
-      });
-    } else {
-      this.setState({
-        username: 'user'
-      });
+      fullName: '',
+      howToFilter: '',
+      rawData: data,
+      filteredData: data
     }
   }
-  render() {
-    return (
-      <div>
-        <Header potato={this.state.username}/>
-        <form onSubmit={this.handleFormSubmitted}>
-          <label htmlFor="name">Name</label>
 
-          <input id="name" name="name" type="text" onInput={this.handleUsernameTyped} />
-          <br />
-          <label htmlFor="dob">Date of Birth</label>
-          <input type="date" name="dob" id="dob" />
-          <br />
-          <input type="submit" />
-        </form>
-        <Container>
-          <Form>
-            <Form.Group controlId="bootstrapName">
-              <Form.Label>Name</Form.Label>
-              <Form.Control type="text" onInput={this.handleUsernameTyped} />
-            </Form.Group>
-          </Form>
-        </Container>
-      </div>
+  handleSubmit = (event) => {
+    event.preventDefault();
+    let fullName = `${event.target.firstname.value} ${event.target.lastname.value}`;
+    console.log(fullName);
+
+  this.setState({
+    fullName: fullName,
+    // howToFilter: event.target.selected.value
+  });
+  // setState is slow so you can't use it right away
+  // console.log(this.state.howToFilter); // odd, even, all
+  if (this.state.howToFilter === 'even') {
+    let newData = this.state.rawData.filter((num) => num % 2 === 0);
+    console.log(newData);
+    this.setState({filteredData: newData})
+
+  } else if (this.state.howToFilter === 'odd') {
+    let newData = this.state.rawData.filter((num) => num % 2 === 1);
+    console.log(newData);
+    this.setState({filteredData: newData})
+  } else {
+    // "all"
+    this.setState({filteredData: this.state.rawData});
+  }
+
+
+  // access a value from a Form.Group
+  console.log(event.target.username.value);
+}
+
+handleSelectedChange = (event) => {
+  let selected = event.target.value;
+  this.setState({
+    howToFilter: selected 
+  });
+}
+  
+  render() {
+
+    let numbers = this.state.filteredData.map((num, idx) => {
+      return <ListGroup.Item key={idx}>{num}</ListGroup.Item>
+    })
+
+    return (
+      <>
+      <header>
+      <h1>Forms in React</h1>
+      </header>
+      <main className="main">
+        <Form onSubmit={this.handleSubmit}>
+          {/* "for" means a for loop in JS; we have to use htmlFor to link a label and input for HTML screen readers */}
+          <Form.Label htmlFor="firstname">First Name</Form.Label>
+          <Form.Control id="firstname" type="text" name="firstname"/>
+
+          <Form.Label>Last Name
+            <Form.Control type="text" name="lastname"/>
+          </Form.Label>
+
+{/* controlId is how we access the values via our event handlers */}
+{/* does not need an htmlFor, an id or name */}
+          <Form.Group controlId="username">
+            <Form.Label>username</Form.Label>
+            <Form.Control type="text"/>
+          </Form.Group>
+
+          <Form.Label>Select Numbers
+          <Form.Select name="selected">
+            <option value="all">All</option>
+            <option value="odd">Odd</option>
+            <option value="even">Even</option>
+          </Form.Select>
+          </Form.Label>
+
+          <Button type="submit">Submit</Button>
+        </Form>
+      </main>
+      {/* ListGroup is our UL */}
+      <ListGroup>
+        {numbers}
+      </ListGroup>
+      </>
     )
   }
 }
+
 export default App;
